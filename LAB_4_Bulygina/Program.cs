0567b6f070df738b10c270114d459cec11465774
Светлана Булыгина <svetlanabulygina9@gmail.com>
2019-12-25 05:16:25 +0300

    Добавьте файлы проекта.

---
+++ LAB_4_Bulygina/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAB_4_Bulygina
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
+
